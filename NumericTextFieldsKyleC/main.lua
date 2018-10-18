8dd352865af7c39cf408a69520c30abc9463f0bd
Kylechristensen8842 <43419336+Kylechristensen8842@users.noreply.github.com>
2018-10-18 15:01:41 -0400

    added numeric text field

--- NumericTextFieldsKyleC/main.lua
+++ NumericTextFieldsKyleC/main.lua
@@ -8,7 +8,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --sets the background colour 
-display.setDefault("background", 140/255, 247/255, 123/255)
+display.setDefault("background", 44/255, 202/255, 214/255)
 
 --Local variables
 
@@ -22,14 +22,22 @@ local randomNumber2
 local userAnswer
 local incorrectAnswer
 local correctAnswer
+local pointsTextObject
+local numberPoints = 0
 local randomOperator
+
+-- correct sound
+local correctSound = audio.loadSound("Sounds/correctSound.mp3")
+local correctSoundChannel
+
+local wrongSound = audio.loadSound("Sounds/wrongSound.mp3")
 
 -- local functions 
 local function AskQuestion()
 	--generate 2 random numbers between a max. and a min. number
 	randomOperator = math.random(0,3)
-	randomNumber1 = math.random(0,10)
-	randomNumber2 = math.random(0,10)
+	randomNumber1 = math.random(10,20)
+	randomNumber2 = math.random(10,20)
 
  
  	if (randomOperator == 1) then
@@ -56,6 +64,12 @@ end
 local function HideCorrect()
 	correctObject.isVisible = false
 	AskQuestion()
+
+end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
+	AskQuestion()
 end
 
 local function NumericFieldListener( event )
@@ -64,7 +78,7 @@ local function NumericFieldListener( event )
 	if ( event.phase == "began" ) then
 
 		--clear text field 
-		event.target.text = ""
+		event.target.text = "" 
 
 	elseif event.phase == "submitted" then
 
@@ -75,36 +89,50 @@ local function NumericFieldListener( event )
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true 
 			incorrectObject.isVisible = false
+			correcSoundChannel = audio.play(correctSound)
 			timer.performWithDelay(2000,HideCorrect)
+			numberPoints = numberPoints + 1
+
+        -- create increasing points in the text object
+			 pointsTextObject.text = "Points = ".. numberPoints
+
 		else 
 			correctObject.isVisible = false 
 			incorrectObject.isVisible = true
-			timer.performWithDelay(2000,HideCorrect)
+			wrongSoundChannel = audio.play(wrongSound)
+			timer.performWithDelay(2000,HideIncorrect)
 
 
 		end
+		event.target.text = "" 
+
 	end
 end
+
+
+-- create points box adn make it visible
+pointsTextObject = display.newText( "Points = ".. numberPoints, 800, 385, nil, 50 )
+pointsTextObject:setTextColor(255/255, 42/255, 69/255)
 
 --object Creation 
 
 -- display a question and sets the color 
-questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50)
-questionObject:setTextColor(155/255, 42/255, 198/255)
+questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 80)
+questionObject:setTextColor(0/255, 0/255, 0/255)
 
 -- create the correct  text object and make it invisible
-correctObject = display.newText("correct boi!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
-correctObject:setTextColor(155/255, 42/255, 198/255)
+correctObject = display.newText("correct boi!", display.contentWidth/2, 250, nil, 50)
+correctObject:setTextColor(69/255, 69/255, 255/255)
 correctObject.isVisible = false
 
 -- create the incorrect  text object and make it invisible
-incorrectObject = display.newText("incorrect boi!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
-incorrectObject:setTextColor(155/255, 42/255, 198/255)
+incorrectObject = display.newText("incorrect boi!", display.contentWidth/2, 250, nil, 50)
+incorrectObject:setTextColor(69/255, 255/255, 69/255)
 incorrectObject.isVisible = false
 
 -- create numeric field 
 
-numericField = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField = native.newTextField(display.contentWidth/3, display.contentHeight*2/3, 150, 150)
 numericField.inputType = "number"
 
 --add the event listener fo the numeric field

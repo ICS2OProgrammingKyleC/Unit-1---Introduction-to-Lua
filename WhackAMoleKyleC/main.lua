557e81996edad0221cbf7e543631e4d4f93d84a3
Kylechristensen8842 <43419336+Kylechristensen8842@users.noreply.github.com>
2018-11-01 14:07:40 -0400

    boom

--- WhackAMoleKyleC/main.lua
+++ WhackAMoleKyleC/main.lua
@@ -9,7 +9,7 @@ display.setStatusBar(display.HiddenStatusBar)
 -- display the back ground
 display.setDefault( "background", 255/255, 86/255,  117/255 )
 
--- creating  Midoriya
+-- creating  Mole
 local Mole = display.newImage("Images/Mole.png", 0, 0)
 	
 	--setting position 
@@ -19,7 +19,7 @@ local Mole = display.newImage("Images/Mole.png", 0, 0)
 	-- setting the scale for the mole
 	Mole:scale(0.5, 0.5)
 
-	-- making Midoriya visible
+	-- making Mole visible
 	Mole.isVisible = true
 
 local scoreNumber = 0
@@ -27,7 +27,7 @@ local scoreObject = display.newText( "Score = "..scoreNumber, 150, 100, nil, 60)
 scoreObject:setTextColor(43/255, 16/255, 223/255)
 scoreObject.isVisible = 1
 
-
+-- this function makes the mole pop up and visible
 function PopUp()
  Mole.x = math.random(0, display.contentWidth)
  Mole.y = math.random(0, display.contentHeight)
@@ -36,19 +36,23 @@ function PopUp()
  timer.performWithDelay(800, Hide)
 end
 
+--this make the mole have a delay when it pops up 
 function PopUpDelay( )
  timer.performWithDelay(800, PopUp)
 end
 
+-- this hides the mole
 function Hide( )
  Mole.isVisible = false
  PopUpDelay()
 end
 
+-- this function starts the game
 function GameStart( )
  PopUpDelay()
 end
 
+--this function is for when u click on the mole
 function Whacked( event )
 if (event.phase == "began") then
   scoreNumber = scoreNumber + 1
@@ -58,6 +62,7 @@ if (event.phase == "began") then
  end
 end
 
+-- this is the event listener
 Mole:addEventListener( "touch", Whacked)
 
 -----------START GAME--------------------
